619e0e24c30e50a502112aa1d168347caf87f4f6
asmayn <102341518+asmayn@users.noreply.github.com>
2024-05-19 20:28:12 +0700

    Update ShowBook.vue

--- src/pages/ShowBook.vue
+++ src/pages/ShowBook.vue
@@ -1,31 +1,32 @@
 <template>
-<div>
-    <li>
-    <h1>{{ bookName }}</h1>
-    <button @click="showDetails()">more details</button>
-    <transition name="fade">
-        <div v-show="isVisible">
-            <h3>{{ bookAuthor }}</h3>
-            <h3>Category: {{ bookCategory }}</h3>
-            <h3>{{ bookDesc }}</h3>
-            <h3> {{ bookCon }}</h3>
-        </div>
-    </transition>
-    </li>
-    <!-- <h1>{{ bookAuthor }}</h1>
+    <div>
+        <li>
+            <!-- {{bookImage }} -->
+            <h1>{{ bookName }}</h1>
+            <button @click="showDetails()">more details</button>
+            <transition name="fade">
+                <div v-show="isVisible">
+                    <h3>{{ bookAuthor }}</h3>
+                    <h3>Category: {{ bookCategory }}</h3>
+                    <h3>{{ bookDesc }}</h3>
+                    <h3> {{ bookCon }}</h3>
+                </div>
+            </transition>
+        </li>
+        <!-- <h1>{{ bookAuthor }}</h1>
     <h1>{{ bookCategory }}</h1>
     <h1>{{ bookTitle }}</h1>
     <h1>{{ bookDesc }}</h1> -->
 
-</div>
+    </div>
 </template>
 <script>
 // import { defineComponent } from '@vue/composition-api'
-import{mapMutations} from 'vuex';
-import{LOADING_SPINNER_SHOW_MUTATION} from '../store/storeconstants';
+import { mapMutations } from 'vuex';
+import { LOADING_SPINNER_SHOW_MUTATION } from '../store/storeconstants';
 
-export default{
-    name:"ShowBook",
+export default {
+    name: "ShowBook",
     // data(){
     //     return {
     //         // donationCategory:'',
@@ -37,67 +38,73 @@ export default{
     //     }
     // },
 
-    props:{
+    props: {
         // "bookName",
         // "bookAuthor",
         // "bookCategory",
         // "bookTitle",
         // "bookDesc",
 
-        // bookId:{
-        //     type:Number,
-        //     requireed:true
-
-        // },
-        bookName:{
-            type: String,
-            requireed:true
-        },
-        bookAuthor:{
-            type: String,
+        bookId: {
+            type: Number,
+            requireed: true
 
         },
-        bookCategory:{
+
+        bookName: {
+            type: String,
+            requireed: true
+        },
+        bookAuthor: {
+            type: String,
+
+        },
+        bookCategory: {
             type: String,
             // requireed:true
-        },        
-        bookDesc:{
+        },
+        bookDesc: {
             type: String,
 
         },
-        bookCon:{
+        bookCon: {
             type: String,
 
         },
-        isVisible:{
-            typr: Boolean
-        }
+        isVisible: {
+            type: Boolean
+        },
+
+
     },
 
-    
+
     methods: {
         ...mapMutations({
             showLoading: LOADING_SPINNER_SHOW_MUTATION,
-    }),
-        showDetails(){
+        }),
+        showDetails() {
+            console.log("Emitting show event with bookId:", this.bookId);
+            this.$emit("show", this.bookId);
             // console.log(bookId);
-            this.$emit("show");
+            //this.$emit("show");
         }
     }
 };
 </script>
 <style scoped>
-li{
-    margin:1rem 0;
+li {
+    margin: 1rem 0;
     font-size: 1.25rem;
     font-weight: bold;
-    background: #a2c6f4 ;
+    background: #a2c6f4;
     padding: 0.5rem;
-    color:rgb(0, 51, 127);
+    color: rgb(0, 51, 127);
     border-radius: 25px;
 }
-button{
-    font:inherit;
+
+button {
+    font: inherit;
     cursor: pointer;
     border: 1px solid #ffc0e9;
     background: #c55ca0;
@@ -105,12 +112,14 @@ button{
     padding: 0.5rem 1rem;
     box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
 }
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 0.5s;
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s;
 }
-.fade-enter, .fade-leave-to {
-  opacity: 0;
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
 }
 </style>
-
-    
